92534ca434f59af64902e36cf6aebde4056bac0f
John Benedict Nuqui <nbenedictp@gmail.com>
2026-08-10 00:17:35 +0800

    added folder for library and samples

---
+++ Library/_segmentCounter/_segmentCounter.cpp
@@ -0,0 +1,5 @@
+#include "_segmentCounter.h"
+
+int addTwoInts(int a, int b){
+  return a + b;
+}
---
+++ Library/_segmentCounter/_segmentCounter.h
@@ -0,0 +1,9 @@
+#ifndef _SEGMENTCOUNTER_H
+#define _SEGMENTCOUNTER_H
+#included <Arduino.h>
+
+
+int addTwoInts(int a, int b);
+
+
+#endif
